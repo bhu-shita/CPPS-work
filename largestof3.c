db65ae4290a870bc647d7dc4c1c6d6780c18d84d
bhu-shita <157133692+bhu-shita@users.noreply.github.com>
2024-09-26 17:28:22 +0530

    largestof3.c

--- largestof3.c
+++ largestof3.c
@@ -1,15 +1,13 @@
-//Find the Largest of Three Numbers Using a Function
-//26-09-2024
+//To find the Largest of Three Numbers Using a Function
+//roll no-27, date:26-09-2024
 
 #include <stdio.h>
-
 
 float findLargest(float a, float b, float c);
 
 int main() {
     float num1, num2, num3, largest;
 
-    // Taking user input for three numbers
     printf("Enter three numbers:\n");
     printf("Number 1: ");
     scanf("%f", &num1);
@@ -18,10 +16,8 @@ int main() {
     printf("Number 3: ");
     scanf("%f", &num3);
 
-    // Finding the largest number
     largest = findLargest(num1, num2, num3);
 
-    // Displaying the result
     printf("The largest number is: %.2f\n", largest);
 
     return 0;
