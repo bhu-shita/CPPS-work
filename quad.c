80fbbd785320b672d4e26c3858928e2016ce32ca
bhu-shita <157133692+bhu-shita@users.noreply.github.com>
2024-09-27 11:40:57 +0530

    quad.c

--- quad.c
+++ quad.c
@@ -1,16 +1,11 @@
 //to calculate a function that will calculate and display the real roots of the quadratic equation ax^2+bx+c=0 using the quadratic formula
 //roll no 27
+
 #include <stdio.h>
 #include <math.h>
-
-
 void findRoots(float a, float b, float c) {
     float discriminant, root1, root2, realPart, imaginaryPart;
-    
-    
-    discriminant = b*b - 4*a*c;
-    
-    
+    discriminant = b*b - 4*a*c;    
     if (discriminant > 0) {
         
         root1 = (-b + sqrt(discriminant)) / (2*a);
@@ -19,12 +14,10 @@ void findRoots(float a, float b, float c) {
         printf("Root 1 = %.2f\n", root1);
         printf("Root 2 = %.2f\n", root2);
     } else if (discriminant == 0) {
-        
         root1 = -b / (2*a);
         printf("Roots are real and equal:\n");
         printf("Root 1 = Root 2 = %.2f\n", root1);
     } else {
-        
         realPart = -b / (2*a);
         imaginaryPart = sqrt(-discriminant) / (2*a);
         printf("Roots are complex and different:\n");
@@ -32,16 +25,10 @@ void findRoots(float a, float b, float c) {
         printf("Root 2 = %.2f - %.2fi\n", realPart, imaginaryPart);
     }
 }
-
 int main() {
     float a, b, c;
-    
-    
     printf("Enter coefficients a, b, and c: ");
     scanf("%f %f %f", &a, &b, &c);
-    
-    
     findRoots(a, b, c);
-    
     return 0;
 }
